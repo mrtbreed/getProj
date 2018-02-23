8437b4aa116558449d22a2bf0ed180dcc7e90dcf
mrtbreed <36203472+mrtbreed@users.noreply.github.com>
2018-02-22 17:42:08 -0800

    Add files via upload

---
+++ Controllers/AccountControler.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportsStore.WebUI.Infrastructure.Abstract;
+using SportsStore.WebUI.Models;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class AccountControler : Controller
+    {
+        IAuthProvider authProvider;
+
+        public AccountControler(IAuthProvider auth)
+        {
+            authProvider = auth;
+        }
+        public ViewResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                if (authProvider.Authenticate(model.Username, model.Password))
+                {
+                    return Redirect(returnUrl ?? Url.Action("Index", "Admin"));
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Incorrect UN or PWRD");
+                    return View();
+                }
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+    }
+}
